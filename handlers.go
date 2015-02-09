6cfc08a35a09e9878a45e9cbdec9043ad77494f8
Adam <electronicbattleweapon7@gmail.com>
2015-02-09 19:51:02 +0000

    rm currentUser jazz

--- handlers.go
+++ handlers.go
@@ -3,55 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/markbates/goth/gothic"
 )
 
-type User struct {
-	Email       string
-	Name        string
-	NickName    string
-	Description string
-	UserID      string
-	AvatarURL   string
-	Location    string
-	AccessToken string
-}
-
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	view.HTML(w, 200, "index", getCurrentUser)
+	view.HTML(w, 200, "index", nil)
 }
 
 func CallbackHandler(w http.ResponseWriter, r *http.Request) {
 	user, err := gothic.CompleteUserAuth(w, r)
+
 	if err != nil {
 		fmt.Fprintln(w, err)
 		return
 	}
 
-	newUser := User{
-		user.Email,
-		user.Name,
-		user.NickName,
-		user.Description,
-		user.UserID,
-		user.AvatarURL,
-		user.Location,
-		user.AccessToken,
-	}
-
-	setCurrentUser(w, r, user.UserID)
-
-	view.JSON(w, http.StatusOK, newUser)
-}
-
-func setCurrentUser(w http.ResponseWriter, r *http.Request, id string) {
-	session, _ := store.Get(r, os.Getenv("SESSION_NAME"))
-	session.Values["current_user"] = id
-	session.Save(r, w)
-}
-func getCurrentUser(r *http.Request) string {
-	session, _ := store.Get(r, os.Getenv("SESSION_NAME"))
-	return session.Values["current_user"].(string)
+	view.JSON(w, http.StatusOK, user)
 }
